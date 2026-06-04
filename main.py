8f04495e773ec961513e4e9678e628a4e5bdd842
steven-rivera <83838429+steven-rivera@users.noreply.github.com>
2026-06-04 13:16:11 -0700

    added support for decoding bencoded dictionaries

--- main.py
+++ main.py
@@ -10,32 +10,35 @@ import sys
 # - decode_string(b"5:hello") -> b"hello"
 # - decode_string(b"10:hello12345") -> b"hello12345"
 
+
 def decode_string(bencoded_value: bytes, start: int) -> tuple[bytes, int]:
     colon_index = bencoded_value.find(b":", start)
     if colon_index == -1:
         raise ValueError("Invalid encoded value: expected ':'")
-    
+
     length = bencoded_value[start:colon_index]
     if not length.isdigit():
-        raise  ValueError(f"Invalid encoded value: expected integer got '{length}'")
+        raise ValueError(f"Invalid encoded value: expected integer got '{length}'")
     length = int(length)
 
     string_start = colon_index + 1
     string_end = string_start + length
 
-    return bencoded_value[string_start: string_end], string_end
+    return bencoded_value[string_start:string_end], string_end
+
 
 # Examples:
 #
 # - decode_integer(b"i52e") -> 52
 # - decode_integer(b"i-52e") -> -52
 
+
 def decode_integer(bencoded_value: bytes, start: int) -> tuple[int, int]:
     e_char_index = bencoded_value.find(b"e", start)
     if e_char_index == -1:
         raise ValueError("Invalid encoded value: expected 'e'")
-    
-    integer = bencoded_value[start + 1: e_char_index]
+
+    integer = bencoded_value[start + 1 : e_char_index]
 
     try:
         integer = int(integer)
@@ -44,36 +47,63 @@ def decode_integer(bencoded_value: bytes, start: int) -> tuple[int, int]:
     else:
         return integer, e_char_index + 1
 
+
 # Examples:
 #
 # - decode_list(b"l5:helloi52ee") -> ["hello",52]
 
+
 def decode_list(bencoded_value: bytes, start: int) -> tuple[list, int]:
     decoded_list = []
-    
+
     # Start search after 'l'
     curr = start + 1
     while chr(bencoded_value[curr]) != "e":
-        value, end = decode_bencode(bencoded_value, start=curr) 
+        value, curr = decode_bencode(bencoded_value, start=curr)
         decoded_list.append(value)
-        curr = end
-    
+
     return decoded_list, curr + 1
 
-def decode_bencode(bencoded_value: bytes, start: int = 0) -> tuple[bytes | int | list, int]:
+
+# Examples:
+#
+# - decode_dictionary(b"d3:foo3:bar5:helloi52ee") -> {"foo":"bar", "hello": 52}
+
+
+def decode_dictionary(bencoded_value: bytes, start: int) -> tuple[dict, int]:
+    decoded_dict = {}
+
+    # Start search after 'd'
+    curr = start + 1
+    while chr(bencoded_value[curr]) != "e":
+        key, curr = decode_bencode(bencoded_value, start=curr)
+        if not isinstance(key, bytes):
+            raise ValueError("Keys must be strings")
+
+        if curr >= len(bencoded_value) or chr(bencoded_value[curr]) == "e":
+            raise ValueError("Expected value for key in dictionary")
+
+        value, curr = decode_bencode(bencoded_value, start=curr)
+        decoded_dict[key.decode()] = value
+
+    return decoded_dict, curr + 1
+
+
+def decode_bencode(
+    bencoded_value: bytes, start: int = 0
+) -> tuple[bytes | int | list | dict, int]:
     if chr(bencoded_value[start]).isdigit():
-        byte_string, end = decode_string(bencoded_value, start)
-        return byte_string, end
-    
+        return decode_string(bencoded_value, start)
+
     if chr(bencoded_value[start]) == "i":
-        integer, end = decode_integer(bencoded_value, start)
-        return integer, end
+        return decode_integer(bencoded_value, start)
 
-    if chr(bencoded_value[0]) == "l":
-        decoded_list, end = decode_list(bencoded_value, start)
-        return decoded_list, end
+    if chr(bencoded_value[start]) == "l":
+        return decode_list(bencoded_value, start)
 
-        
+    if chr(bencoded_value[start]) == "d":
+        return decode_dictionary(bencoded_value, start)
+
     raise NotImplementedError("Only strings are supported at the moment")
 
 
@@ -95,7 +125,7 @@ def main():
                 return data.decode()
 
             raise TypeError(f"Type not serializable: {type(data)}")
-        
+
         decoded, _ = decode_bencode(bencoded_value)
         print(json.dumps(decoded, default=bytes_to_str))
     else:
